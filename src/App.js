e4c2198b9eb2da9e8004a11077330aea87a44b6e
ZakariyaeOuazzanix <zakariyaeouazzani90@gmail.com>
2024-07-25 17:02:44 +0100

    started with the board

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+/*import logo from './logo.svg';
+import './App.css';*/
+
+function Square(){
+  return(
+    <div className="w-10 h-10 bg-gray-500"></div>
+    
+  );
+}
+
+function Board(){
+  const squares = Array(9).fill(null);
+  return(
+    <>
+      
+    <div className="grid grid-cols-3 grid-rows-3 gap-0 w-max">
+      {
+      squares.map((elt, idx)=>
+        <Square key={idx}/>
+      )}
+    </div>
+      
+
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="m-10">
+        <Board />
+          
+      </div>
+      
+    
+    </>
+    
   );
 }
 
